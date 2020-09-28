6faae47417e64dc81450c9ab0970b270b52be60b
Jona <me@datdenkikniet.nl>
2020-09-28 13:35:20 +0200

    Fix cuda (hopefully)

--- calcpi-cuda.cu
+++ calcpi-cuda.cu
@@ -1,10 +1,12 @@
-__global__ void bruh(){
+extern "C" {
+__global__ void bruh() {
 
 }
 
-double calc_pi(int iterations){
-    for (int i = 0; i < iterations; i++){
+double calc_pi(int iterations) {
+    for (int i = 0; i < iterations; i++) {
         bruh<<<3, 4>>>();
     }
     return 3.14;
 }
+};
