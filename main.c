869c9c8aabfa834774e2c50d870b9939599bbada
jd222pg <jd222pg@student.lnu.se>
2020-09-28 11:30:04 +0200

    Add command line options, help menu, more specific printouts, comments

--- main.c
+++ main.c
@@ -7,11 +7,28 @@
 
 #define PI 3.14159265358979323846264
 
+// For this task, 4x oversubscription seems to actually improve program speed? Theorize about non-uniform memory access
+// causing lots of memory-waiting. Run tests!
+
+const int default_iterations = 192000000;
+
+void print_help(char *cmd) {
+    printf("Program for calculating PI in parallel, using OpenMP.\n");
+    printf("Command format: %s [options]\n", cmd);
+    printf("Available options:\n");
+    printf("-h        Show this help menu\n");
+    printf("-v        Enable verbose printing\n");
+    printf("-t [num]  Set the amount of threads to use. Use 0 for maximum available.\n"
+                  "          Default: amount of threads available on the system (Here: %d)\n", omp_get_max_threads());
+    printf("-i [num]  Set the amount of iterations to run. Default: %d\n", default_iterations);
+}
+
 int main(int argc, char *argv[]) {
-    int iterations = 192000000;
+    int iterations = default_iterations;
     int threads = omp_get_max_threads();
+    int quiet = 1;
     int option;
-    while ((option = getopt(argc, argv, "i:t:")) != -1) {
+    while ((option = getopt(argc, argv, ":i:t:vh")) != -1) {
         switch (option) {
             case 'i': {
                 char end_char;
@@ -31,13 +48,21 @@ int main(int argc, char *argv[]) {
                     printf("Invalid thread count\n");
                     return 1;
                 }
-                if (threads == 0){
+                if (threads == 0) {
                     threads = omp_get_max_threads();
                 }
                 break;
             }
+            case 'v':
+                quiet = 0;
+                break;
+            case 'h':
+                print_help(argv[0]);
+                return 0;
+            case '?':
             default: {
-                printf("Unknown option %c\n", option);
+                printf("Unknown option %c\n", optopt);
+                print_help(argv[0]);
                 return 1;
             }
         }
@@ -62,7 +87,15 @@ int main(int argc, char *argv[]) {
 
     mypi *= m;
     stoptimer(&timer);
-    printf("Computation took %li.%03li seconds\n", timer.seconds, timer.nanos / 1000000);
-    printf("     MyPI = %.20lf\n", mypi);
-    printf("MyPI - PI = %.20lf\n", (mypi - PI));
+    if (!quiet) {
+        printf("Computation took %li.%06li seconds\n", timer.seconds, timer.nanos / 1000);
+        printf("Performed %d iterations using %d threads.\n", iterations, threads);
+        printf("     MyPI = %.20lf\n", mypi);
+        printf("MyPI - PI = %.20lf\n", (mypi - PI));
+    } else {
+        // Output format:
+        // s.micros, iter, thrd, pi, diff
+        printf("%li.%06li, %d, %d, %.20lf, %.20lf\n", timer.seconds, timer.nanos / 1000, iterations, threads, mypi,
+               mypi - PI);
+    }
 }
